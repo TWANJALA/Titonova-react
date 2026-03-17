4afbd36633768284f97c167d4a19ab94a6e06c15
Titus Wanjala <tituswanjala89@gmail.com~>
2026-03-17 07:36:11 -0500

    Add explicit hosting API routes and shared gateway proxy

---
+++ api/hosting/unpublish.js
@@ -0,0 +1,7 @@
+import { config, proxyToGateway } from "../_gatewayProxy.js";
+
+export { config };
+
+export default async function handler(req, res) {
+  return proxyToGateway(req, res, "/api/hosting/unpublish");
+}
